acd556c9239fbd1842b768a897ad3b3d744a2060
chenweiqiang <chenweiqiang@lightinthebox.com>
2016-03-03 18:09:34 +0800

    1688商品和商户的抓取都增加了字段

--- 1688products.py
+++ 1688products.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
+# 2016/3/3 增加字段: tag star reviews total_sales
+# 可能遭到屏蔽 只能适当时候运行
+
 import httplib2
 import uuid
 import os
+import random
+import time
 from pyquery import PyQuery
 from utils import extractNum
 
 class Product:
     def __init__(self):
         self.attrs = {}
-        self.attrs['category'] = "宠物衣服";
+        self.attrs['category'] = "窗帘";
         
     def __getitem__(self, key):
         if self.attrs.has_key(key):
@@ -18,7 +23,8 @@ class Product:
     def __setitem__(self, key, value):
         self.attrs[key] = value;
 
-def fetchContent(url, saveFile=True):
+def fetchContent(url, saveFile=False):
+#     sleep();
     h = httplib2.Http()
     response, content = h.request(url)
     print response
@@ -29,6 +35,10 @@ def fetchContent(url, saveFile=True):
         fw.write(content.encode("utf-8")) #Q: unicode写不进去 utf-8可以正常写
         fw.close()
     return content
+
+def sleep():
+    times = random.uniform(2, 5)
+    time.sleep(times)
 
 def parsePage(content):
     doc = PyQuery(content)
@@ -45,11 +55,28 @@ def parsePage(content):
         p['merchant'] =  nodeQ('dd.company').text()
         p['merchant_url'] = "http:" + nodeQ('dd.company > a').eq(-1).attr('href')
         p['monthly_sales']= nodeQ('dd.data > span.sold-out > em.num').text();
+        #在产品列表页面增加一个字段
+        p['tags'] = nodeQ("dd.signage > span").text()
+        parseProductPage(p)
         productList.append(p)
+        #return productList #测试
     return productList
 
+def parseProductPage(product):
+    """进入商品详情页, 抓取四个新字段
+       delivery reviews star total_sales
+    """
+    if product['product_url']:
+       content = fetchContent(product['product_url'])
+       doc=PyQuery(content)
+       #product['delivery'] = doc("div.cost-entries-type > p > em.value").text() 运费JS动态 解决不了
+       product['reviews'] = doc('p.satisfaction-number > a > em.value').text()
+       product['star'] = doc('p.star-level > i').attr("class")
+       product['total_sales'] = doc('p.bargain-number > a > em.value').text() 
+    return product
+
 def persistance(objList):
-    fields = ['category', 'name', 'price', 'img_url', 'product_url', 'city', 'merchant', 'merchant_url', 'monthly_sales']
+    fields = ['category', 'name', 'price', 'img_url', 'product_url', 'city', 'merchant', 'merchant_url', 'monthly_sales', 'tags', 'reviews', 'star', 'total_sales']
     first_line = '\t'.join(fields) + '\n'
     fw = open(os.path.basename(__file__).split('.')[0] + "-result.csv", 'w')
     fw.write(first_line)
@@ -60,6 +87,8 @@ def persistance(objList):
                 obj[key] = obj[key].encode("utf-8")
             if (type(obj[key]) is int):
                 obj[key] = str(obj[key])
+            if not obj[key]: #NoneType
+                obj[key] = ''
             datas.append(obj[key])
         line = '\t'.join(datas) + '\n'
         fw.write(line)
@@ -88,6 +117,7 @@ def main(start_url):
     print "WELL DOME"
 
 if __name__ == '__main__':
-    start_url = "https://ye.1688.com/chanpin/-b3e8ceefd2c2b7fe.htm?spm=a360i.cyd0017.0.0.zk3ee8&homeType=1&analy=n&newProduct=1&sortType=MLR_PAY_COUNT&sortOrder=DESC#filt"
+    #start_url = "https://ye.1688.com/chanpin/-b3e8ceefd2c2b7fe.htm?spm=a360i.cyd0017.0.0.zk3ee8&homeType=1&analy=n&newProduct=1&sortType=MLR_PAY_COUNT&sortOrder=DESC#filt"
+    start_url = "http://ye.1688.com/chanpin/-b4b0c1b1.htm?spm=a360i.cyd0017.0.0.Wl5YRp&homeType=1&analy=n&sortType=MLR_PAY_COUNT&sortOrder=DESC#filt"
     main(start_url)
     
--- 1688suppliers.py
+++ 1688suppliers.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
+#使用json抓取联系页面的满意度 但是返回的数据不准 好多为0
+
 import httplib2
 import uuid
 import os
+import re
+import json
 from pyquery import PyQuery
 from utils import extractNum
 
 class Merchant:
     def __init__(self):
         self.attrs = {}
-        self.attrs['category'] = "宠物衣服";
+        self.attrs['category'] = "窗帘";
         
     def __getitem__(self, key):
         if self.attrs.has_key(key):
@@ -45,20 +49,69 @@ def parsePage(content):
         m['satisfaction_rate'] = nodeQ("dl[class='info basic-info'] > dd.probability > span > em.value").eq(0).text()
         m['retention_rates'] = nodeQ("dl[class='info basic-info'] > dd.probability > span > em.value").eq(1).text()
         m['weekly_sales'] = extractNum(nodeQ('dd > span.num').text())
+        #增加以下字段 诚信经营几年 是否支持支付宝 是否保障
+        m['years'] = nodeQ('dd.service > a.icon-goldcxt > em').text()
+        m['isAlipay'] = u'是' if nodeQ('dd.service > a.icon-alipay') else u"否"
+        m['isTrust'] = u'是' if nodeQ('dd.service > a.icon-trust') else u"否"
+        #进入商户页面抓取信息
+        parseSupplierContactPage(m)
         merchantList.append(m)
     return merchantList
 
+def parseSupplierContactPage(m):
+    #http://yongjia.1688.com/shop/aofudianqi/page/contactinfo.htm?
+    if m['url'].find('\?') > 0:
+        """do nothing"""
+    else:
+        if m['url'].endswith("/"):
+            m['url'] = m['url'][:-1]
+        m['url'] = m['url'] + '?'
+    #拼出联系页面的url
+    contact_page_url = re.sub("\?.*$", '/page/contactinfo.htm', m['url'])
+    content = fetchContent(contact_page_url)
+    doc = PyQuery(content)
+    #m['satisfication'] = doc('div.detail > div.sat-rate > span.disc > a').text() 动态加载
+    if doc('div.detail > div.trade-medal > span.disc > a.image > img').eq(0).attr('alt'):
+        m['trade_medal'] = doc('div.detail > div.trade-medal > span.disc > a.image > img').eq(0).attr('alt')
+    else:
+        m['trade_medal'] = ''
+    m['supply-grade'] = len(doc('div.detail > div.supply-grade > span.disc > a.image > img'))
+    m['biz-type'] = doc('div.detail > div.biz-type > span').text()
+    if not m['biz-type']:
+        m['biz-type'] = doc('div.detail > div.smt-biz-type > span').text()
+    aList = doc('div.contcat-desc > dl')
+    bList = []
+    for item in aList:
+        itemQ = PyQuery(item)
+        text = itemQ.children('dt').text()
+        #text = re.sub('\s*','', itemQ.children('dt').text()) #摆不平这个 空格去不掉
+        if text.find(u"话") > 0:
+            bList.append(itemQ.children('dd').text())
+    m['contact'] = ', '.join(bList)
+    #根据json数据获取 满意度
+    #http://rate.1688.com/stat/trade/winport.json?memberId=aofudianqi&sati=1
+    #{"data":{"items":[],"sati":{"satisfactionRate":0,"satisfaction":4.6,"remarkCount":428},"dsr":null},"success":true}
+    if re.findall('shop/(.*)/page', contact_page_url):
+        stat_url = 'http://rate.1688.com/stat/trade/winport.json?memberId=' + re.findall("shop/(.*)/page", contact_page_url)[0] + '&sati=1'
+        content2 = fetchContent(stat_url)
+        json_data = json.loads(content2)
+        m['satisfication'] = json_data['data']['sati']['satisfaction']
+    else:
+        m['satisfication'] = ''
+
 def persistance(objList):
-    fields = ['category', 'city', 'name', 'url', 'main_products', 'address', 'satisfaction_rate', 'retention_rates', 'weekly_sales']
+    fields = ['category', 'city', 'name', 'url', 'main_products', 'address', 'satisfaction_rate', 'retention_rates', 'weekly_sales',\
+              'years', 'isAlipay', 'isTrust', 'trade_medal', 'supply-grade', 'biz-type','contact', 'satisfication']
     first_line = '\t'.join(fields) + '\n'
     fw = open(os.path.basename(__file__).split('.')[0] + "-result.csv", 'w')
     fw.write(first_line)
     for obj in objList:
         datas = []
         for key in fields:
+            print key, obj[key]
             if (type(obj[key]) is unicode): #不加外围的括号产生bug
                 obj[key] = obj[key].encode("utf-8")
-            if (type(obj[key]) is int):
+            if (type(obj[key]) is int or type(obj[key]) is float):
                 obj[key] = str(obj[key])
             datas.append(obj[key])
         line = '\t'.join(datas) + '\n'
@@ -88,6 +141,7 @@ def main(start_url):
     print "WELL DOME"
 
 if __name__ == '__main__':
-    start_url = "https://ye.1688.com/qiye/-b3e8ceefd2c2b7fe.htm?spm=a360i.cyd0018.0.0.Tm8FDX&homeType=1&sortType=SALE_QUANTITY#filt"
+    start_url = "http://ye.1688.com/qiye/-b4b0c1b1.htm?spm=a360i.cyd0018.0.0.30HwlG&homeType=1&sortType=SALE_QUANTITY#filt"
+    #start_url = "https://ye.1688.com/qiye/-b3e8ceefd2c2b7fe.htm?spm=a360i.cyd0018.0.0.Tm8FDX&homeType=1&sortType=SALE_QUANTITY#filt"
     main(start_url)
     
